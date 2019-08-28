bc87f7afe873a12e2926e89edcac29818728403e
Charliea835 <Ansellfamily3@gmail.com>
2019-08-28 19:31:25 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('hostname', 'oursystem.mysql.database.azure.com');
-define('user', 'project835@oursystem');
-define('password', 'Homesecure835');
-define('databaseName', 'projectdb');
-
-$connect = mysqli_connect(hostname, user, password, databaseName);
-?>
